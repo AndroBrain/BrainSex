2124ebc14d1a0e8bd4d3f4cf4c79b513faebeb7c
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2022-05-05 09:36:39 +0200

    Added Parcelable implementation to ResultState.kt

--- app/src/main/java/com/androbrain/brainsex/feature/result/ResultState.kt
+++ app/src/main/java/com/androbrain/brainsex/feature/result/ResultState.kt
@@ -1,12 +1,15 @@
 package com.androbrain.brainsex.feature.result
 
+import android.os.Parcelable
 import androidx.annotation.StringRes
 import com.androbrain.brainsex.R
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class ResultState(
     @StringRes val title: Int,
     @StringRes val description: Int,
-) {
+) : Parcelable {
     companion object {
         val Initial
             get() = ResultState(
